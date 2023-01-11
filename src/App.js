c38c514d7128bbbe21327cab9c1bed669bfd2905
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-01-11 21:12:39 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Footer from './components/Footer';
 import Services from './components/Services';
 import Hero from './components/Hero';
 import MailingList from './components/MailingList';
+import AboutButton from './components/AboutButton';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
+      <AboutButton></AboutButton>
       <Services></Services>
       <MailingList/>
       <Footer></Footer>
